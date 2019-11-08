08025e2c84cbc71091ce64ce5cc3e8bd74323561
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2019-11-08 09:32:56 +0100

    db model functions initialized

---
+++ data/db-model.js
@@ -0,0 +1,30 @@
+const db = require("../data/db-config.js");
+
+module.exports = {
+  addResource,
+  getAllResources,
+  addProject,
+  getAllProjects,
+  addTask,
+  getAllTasks
+  
+};
+
+function addResource() {
+    
+}
+function getAllResources() {
+    
+}
+function addProject() {
+    
+}
+function getAllProjects() {
+    
+}
+function addTask() {
+    
+}
+function getAllTasks() {
+    
+}
