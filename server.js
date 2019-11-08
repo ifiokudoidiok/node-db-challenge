0f87fea0f782e9c1e08360f2106b2c8173d21bf5
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2019-11-08 09:25:38 +0100

    server set up and live!

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+// const businessRouter = require('./config/router');
+
+const server = express();
+
+server.use(express.json());
+server.get('/', (req, res) => {
+res.send('<h1>Lets Get this party started!<h1><br /><p> navigate to /api/projects to begin</p>')
+})
+// server.use('/api', businessRouter);
+
+module.exports = server;
