05167341505d7b00378dc1f6a42dd2a57ec8533e
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2019-11-08 11:53:31 +0100

    refactored router, middleware implemented

--- config/router.js
+++ config/router.js
@@ -1,80 +1,138 @@
-const express = require('express');
+const express = require("express");
 
-const model = require('../data/db-model');
+const model = require("../data/db-model");
 
 const router = express.Router();
 
-router.get('/projects', (req, res) => {
-    model.getAllProjects()
+router.get("/projects", getAllProjects);
+router.post("/projects",validateProject, addProject);
+router.get("/resources", getAllResources);
+router.post("/resources",validateResource, addResource);
+router.get("/tasks", getAllTasks);
+router.post("/tasks",validateTask, addTask);
+
+function getAllProjects(req, res) {
+  model
+    .getAllProjects()
     .then(projects => {
       res.json(projects);
     })
     .catch(error => {
-      res.status(500).json({ message: 'Failed to get projects: '+error });
+      res.status(500).json({ message: "Failed to get projects: " + error });
     });
-  });
+}
 
-  router.post('/projects', (req, res) => {
-    model.addProject(req.body)
+function addProject(req, res) {
+  model
+    .addProject(req.body)
     .then(project => {
-        res.status(201).json(project)
+      res.status(201).json(project);
     })
     .catch(error => {
-        
-        res.status(500).json({
-          message: 'Error adding the new project: ' + error.message,
-        });
-      })
-    })
+      res.status(500).json({
+        message: "Error adding the new project: " + error.message
+      });
+    });
+}
 
-  router.get('/resources', (req, res) => {
-    model.getAllResources()
+function getAllResources(req, res) {
+  model
+    .getAllResources()
     .then(resources => {
       res.json(resources);
     })
     .catch(error => {
-      res.status(500).json({ message: 'Failed to get resources: '+error });
+      res.status(500).json({ message: "Failed to get resources: " + error });
     });
-  });
-  router.post('/resources', (req, res) => {
-    model.addResource(req.body)
+}
+function addResource(req, res) {
+  model
+    .addResource(req.body)
     .then(resource => {
-        res.status(201).json(resource)
+      res.status(201).json(resource);
     })
     .catch(error => {
-        
-        res.status(500).json({
-          message: 'Error adding the new resource: ' + error.message,
-        });
-      })
-    })
-
-  router.get('/tasks', (req, res) => {
-    model.getAllTasks()
-    .then(tasks => {
-        res.json(tasks);
-        // if(tasks.task_completed === 0){
-
-        //     res.json({...tasks,task_completed:false });
-        // } else {
-        //     res.json({...tasks,task_completed:true });
-        // }
-    })
-    .catch(error => {
-      res.status(500).json({ message: 'Failed to get tasks: '+error });
+      res.status(500).json({
+        message: "Error adding the new resource: " + error.message
+      });
     });
-  });
-  router.post('/tasks', (req, res) => {
-    model.addTask(req.body)
-    .then(task => {
-        res.status(201).json(task)
+}
+
+function getAllTasks(req, res) {
+  model
+    .getAllTasks()
+    .then(tasks => {
+      res.json(tasks);
     })
     .catch(error => {
-        
-        res.status(500).json({
-          message: 'Error adding the new task: ' + error.message,
-        });
-      })
+      res.status(500).json({ message: "Failed to get tasks: " + error });
+    });
+}
+
+function addTask(req, res) {
+  model
+    .addTask(req.body)
+    .then(task => {
+      res.status(201).json(task);
     })
+    .catch(error => {
+      res.status(500).json({
+        message: "Error adding the new task: " + error.message
+      });
+    });
+}
+
+//Middleware
+function validateProject(req, res, next) {
+    if (Object.keys(req.body).length) {
+        if(req.body.project_name){
+            next();
+
+        }
+        else 
+        {
+            res.status(400).json({ message: "project name is required!" });
+        }
+        
+      } 
+      else 
+      {
+        res.status(400).json({ message: "missing project data" });
+      }
+}
+function validateTask(req, res, next) {
+    if (Object.keys(req.body).length) {
+        if(req.body.task_description && req.body.project_id){
+            next();
+
+        }
+        else 
+        {
+            res.status(400).json({ message: "Description and project_id fields are required!" });
+        }
+        
+      } 
+      else 
+      {
+        res.status(400).json({ message: "missing task data" });
+      }
+}
+function validateResource(req, res, next) {
+    if (Object.keys(req.body).length) {
+        if(req.body.resource_name){
+            next();
+
+        }
+        else 
+        {
+            res.status(400).json({ message: "Resource name is required!" });
+        }
+        
+      } 
+      else 
+      {
+        res.status(400).json({ message: "missing resource data" });
+      }
+}
 
 module.exports = router;
