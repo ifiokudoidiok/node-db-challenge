6f3fa4543cc85058306e5209aafc1531441716f7
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2019-11-08 11:55:51 +0100

    logger added

--- server.js
+++ server.js
@@ -6,8 +6,18 @@ const server = express();
 
 server.use(express.json());
 server.get('/', (req, res) => {
-res.send('<h1>Lets Get this party started!<h1><br /><p> navigate to /api/projects to begin</p>')
+res.send('<h1>Lets Get this party started!<h1><br /><p> navigate to /api/projects , /api/tasks or /api/resources to begin</p>')
 })
-server.use('/api', businessRouter);
+server.use('/api', logger, businessRouter);
+
+
+
+function logger(req, res, next) {
+    console.log(
+      `[${new Date().toISOString()}] ${req.method} to ${req.url} from ${req.host}`
+    );
+  
+    next();
+  }
 
 module.exports = server;
